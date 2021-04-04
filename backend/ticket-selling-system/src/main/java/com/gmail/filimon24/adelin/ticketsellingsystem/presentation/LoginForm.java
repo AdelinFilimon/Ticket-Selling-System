ba9e13e17526fd9cdedb06ed910fa34c57e24c3b
Adelin <filimon.adelin24@gmail.com>
2021-04-04 18:18:36 +0300

    Add LoginForm class

---
+++ backend/ticket-selling-system/src/main/java/com/gmail/filimon24/adelin/ticketsellingsystem/presentation/LoginForm.java
@@ -0,0 +1,22 @@
+package com.gmail.filimon24.adelin.ticketsellingsystem.presentation;
+
+public class LoginForm {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
